3563a2fd5685fe78170532217f3bc8acebbf4b34
fayzah <fmd218@lehigh.edu>
2014-08-27 22:09:53 +0000

    updated HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,26 @@
+//Fayzah Di Natale
+//Hello World Java Program
+//
+// define a class
+public class HelloWorld{
+    
+//  add main method
+    public static void main(String[] args) {
+        
+//      show the message with a box around it 
+        System.out.println("|-----------------------|");
+        System.out.println("|                       |");
+        System.out.println("|     Hello World!!     |");
+        System.out.println("|                       |");
+        System.out.println("|-----------------------|");
+        System.out.println("test");
+    //test
+    }
+}
+
+//Compile the program
+//  javac HelloWorld.java
+//
+//Run the program
+//  java HelloWorld//
+
